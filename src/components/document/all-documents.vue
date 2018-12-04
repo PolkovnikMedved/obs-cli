9f37506c32391890b90f10bdc713dcb73c2f465d
= <viktor.solodoukhin@groups.be>
2018-12-04 11:12:52 +0100

    pagination done

--- src/components/document/all-documents.vue
+++ src/components/document/all-documents.vue
@@ -17,11 +17,8 @@
       <div v-if="documents.content && documents.content.length" class="l-row">
         <div class="l-col-12">
           <div class="l-row">
-            <div class="l-col-1">
-              <span class="s-text s-text--bold">IMG</span>
-            </div>
-            <div class="l-col-7">
-              <span class="s-text s-text--bold">Id - Description</span>
+            <div class="l-col-8">
+              <span class="s-text s-text--bold" style="padding-left:25px">Id - Description</span>
             </div>
             <div class="l-col-3">
               <span class="s-text s-text--bold">Category</span>
@@ -30,17 +27,14 @@
           </div>
 
           <div v-for="(document, index) of documents.content" :key="index" class="l-row">
-            <div class="l-col-1">
-              <span>{{ document.number }}</span>
-            </div>
-            <div class="l-col-7">
-              <span>{{ document.label.frenchLabel }}</span>
+            <div class="l-col-8">
+              <span><chevron-down fill-color="#086cc4"/> {{ document.number }} - {{ document.label.frenchLabel }}</span>
             </div>
             <div class="l-col-3">
               <span>{{ document.category.label.frenchLabel }}</span>
             </div>
             <div class="l-col-1">
-              <span>add</span>
+              <span><plus-icon fill-color="#07b358"/></span>
             </div>
           </div>
 
@@ -50,19 +44,13 @@
                 <paginate
                     :pageCount="documents.totalPages"
                     :container-class="'c-list c-list--inline l-content--center'"
+                    :prev-text="'f'"
+                    :next-text="'n'"
                     :page-link-class="'c-list-item'"
                     :active-class="'c-list-item--is-active'"
                     :click-handler="pageCallback">
-                  <span slot="prevContent">
-                    <svg viewBox="0 0 20 20" id="ic_chevron_left" width="30" height="30">
-                      <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path>
-                    </svg>
-                  </span>
-                  <span slot="nextContent">
-                    <svg viewBox="0 0 20 20" id="ic_chevron_right" width="30" height="30">
-                      <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path>
-                    </svg>
-                  </span>
+                  <span class="wide-icon" slot="prevContent"> <chevron-left fill-color="#086cc4" /> </span>
+                  <span class="wide-icon" slot="nextContent"> <chevron-right fill-color="#086cc4" /> </span>
                 </paginate>
               </nav>
             </div>
@@ -79,6 +67,10 @@ import Paginate from "vuejs-paginate";
 import DocumentsSearch from "../parts/documents-search.vue";
 import { state, show, hide } from "../tools/loader-component";
 import { HTTP } from "../../http-common";
+import ChevronLeft from "vue-material-design-icons/ChevronLeft";
+import ChevronRight from "vue-material-design-icons/ChevronRight";
+import ChevronDown from "vue-material-design-icons/ChevronDown";
+import PlusIcon from "vue-material-design-icons/Plus";
 export default {
   name: "versions",
   data() {
@@ -94,20 +86,23 @@ export default {
     }
   },
   methods: {
-      filter: function(search) {
+    filter: function(search) {
       console.log("From documents: " + JSON.stringify(search));
     },
-      pageCallback: function(pageNum){
+    pageCallback: function(pageNum){
       show();
       HTTP.get("document/all?page=" + pageNum)
         .then(r => {
           this.documents = r.data;
           hide();
         })
-        .catch(e => {this.errors.push(e); hide()});
+        .catch(e => {
+          this.errors.push(e);
+          hide();
+        });
     }
   },
-  components: { DocumentsSearch, Loader, Paginate },
+  components: { DocumentsSearch, Loader, Paginate, ChevronLeft, ChevronRight, PlusIcon, ChevronDown },
   async beforeMount() {
     show();
     HTTP.get("document/all")
