6e4c3a2dffc31703ee7b3eac7d386770c50b79b9
= <viktor.solodoukhin@groups.be>
2019-01-04 11:12:41 +0100

    Change structure element type

--- src/components/element/edit-element-modal.vue
+++ src/components/element/edit-element-modal.vue
@@ -11,6 +11,7 @@
             <div class="c-modal__body">
                 <success-alert @closeSuccess="closeSuccess" :success="updated" :success_message="this.successMessage"/>
                 <error-alert :errors="this.errors" />
+                <loader v-if="visible"></loader>
 
                 <form class="c-form" @submit.prevent="submit">
                     <div class="l-row l-direction--column">
@@ -72,6 +73,22 @@
                             <div class="l-row">
                                 <div class="l-col-3 l-justify--end">
                                     <div class="c-form__field-group">
+                                        <label for="type" class="c-form__label spaced">Type</label>
+                                    </div>
+                                </div>
+
+                                <div class="l-col-6">
+                                    <div class="c-form__field-group full-width">
+                                        <select class="c-form__field spaced full-width" id="type" v-if="structures && structures.length" v-model="element.typeStructure.name">
+                                            <option v-for="structure of structures" :key="structure">{{ structure }}</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="l-row">
+                                <div class="l-col-3 l-justify--end">
+                                    <div class="c-form__field-group">
                                         <label for="description" class="c-form__label spaced">Description</label>
                                     </div>
                                 </div>
@@ -102,31 +119,67 @@ import Close from "vue-material-design-icons/Close.vue";
 import { HTTP } from "../../http-common";
 import SuccessAlert from "../parts/success-alert.vue";
 import ErrorAlert from "../parts/error-alert.vue";
+import Loader from "../tools/loader.vue";
+import { state, show, hide } from "../tools/loader-component";
 
 export default {
   props: { element: { required: true } },
-  components: { Close, ErrorAlert, SuccessAlert },
+  components: { Close, ErrorAlert, SuccessAlert, Loader },
   data() {
     return {
       errors: [],
       updated: false,
-      successMessage: "The element has been updated"
+      successMessage: "The element has been updated",
+      structures: []
     };
   },
+  computed: {
+    visible() {
+      return state.visible;
+    }
+  },
   methods: {
-    submit: function() {
-      HTTP.put("/structure-element/update", JSON.stringify(this.element), { headers: {"Content-Type": "application/json"}})
-        .then(() => {
-          this.updated = true;
-        })
-        .catch(e => this.errors.push(e));
+    submit() {
+      show();
+      if (typeof this.element.typeStructure === "object" ) {
+        this.updateElement(this.element);
+      } else if ( typeof this.element.typeStructure === "string") {
+        HTTP.get("/structure/" + this.element.typeStructure)
+          .then(r => {
+            this.element.typeStructure = r.data;
+            this.updateElement(this.element);
+          })
+          .catch(e => this.errors.push(e));
+      }
+      else {
+        console.log("Element type structure is not object or string: " + JSON.stringify(this.element) + "/");
+        console.log("Type = " + typeof this.element.typeStructure);
+        console.log("Type = " + ( typeof this.element.typeStructure === "object") );
+      }
+      hide();
     },
     close: function() {
       this.$emit("close");
     },
     closeSuccess: function() {
       this.updated = false;
+    },
+    updateElement(element) {
+      HTTP.put("/structure-element/update", JSON.stringify(element), { headers: {"Content-Type": "application/json"}})
+        .then(() => {
+          this.updated = true;
+        })
+        .catch(e => this.errors.push(e));
     }
+  },
+  async beforeMount() {
+    HTTP.get("/structure/all-names")
+      .then(r => {
+        this.structures = r.data;
+      })
+      .catch(e => {
+        this.errors.push(e);
+      });
   }
 };
 </script>
